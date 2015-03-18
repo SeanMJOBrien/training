b9ea9f40c327caad904cd50cb937ec5da9ffa313
seanmjobrien <qlippoth@speakeasy.net>
2015-03-18 13:58:57 -0700

    Adding JAsmine framework and sample unit test.

---
+++ test/spec/test.js
@@ -0,0 +1,12 @@
+/*global describe, it */
+'use strict';
+
+(function () {
+    describe('TEST Give it some context', function () {
+        describe('maybe a bit more context here', function () {
+            it('should run here few assertions', function () {
+
+            });
+        });
+    });
+})();
